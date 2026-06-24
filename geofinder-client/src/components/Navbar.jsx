05aec9356e0295ae5614a87bb525681cd1f5b147
Neema <patelneema93@gmail.com>
2026-06-24 14:18:49 -0400

    add content in Navbar and create css file for Navbar

---
+++ geofinder-client/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import react from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">🌍 GeoFinder</div>
+
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>Explore</li>
+        <li>Favorites</li>
+      </ul>
+
+      <div className="nav-actions">
+        <button className="login-btn">Login</button>
+      </div>
+    </nav>
+  );
+};
