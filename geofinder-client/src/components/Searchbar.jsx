180358b9d944f4459f5f001b76efa5cfe98897b0
Neema <patelneema93@gmail.com>
2026-06-24 15:38:34 -0400

    create Searchbar components

---
+++ geofinder-client/src/components/Searchbar.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+ const Searchbar = () => {
+    return(
+        <>
+        <from>
+        <input />
+        <button>Search</button>
+        </from>
+        </>
+    )
+ } 
