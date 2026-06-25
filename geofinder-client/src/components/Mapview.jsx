874a93ca70da83208e32bb90e3dc4e60742577c9
Neema <patelneema93@gmail.com>
2026-06-25 17:03:43 -0400

    update: Mapview component update with css

--- geofinder-client/src/components/Mapview.jsx
+++ geofinder-client/src/components/Mapview.jsx
@@ -1,25 +1,34 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "./Mapview.css";
 
 const Mapview = ({ coordinates }) => {
   if (!coordinates) {
-    return <p>Search for a location...</p>;
+    return (
+      <div className="map-placeholder">
+        🗺️ Search for a location to view it on the map
+      </div>
+    );
   }
 
   return (
-    <MapContainer
-      center={[coordinates.lat, coordinates.lng]}
-      zoom={13}
-      style={{ height: "500px", width: "100%" }}
-    >
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+    <div className="map-container">
+      <MapContainer
+        center={[coordinates.lat, coordinates.lng]}
+        zoom={13}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-      <Marker position={[coordinates.lat, coordinates.lng]}>
-        <Popup>
-          Lat: {coordinates.lat} <br />
-          Lng: {coordinates.lng}
-        </Popup>
-      </Marker>
-    </MapContainer>
+        <Marker position={[coordinates.lat, coordinates.lng]}>
+          <Popup>
+            <strong>Location</strong>
+            <br />
+            Lat: {coordinates.lat} <br />
+            Lng: {coordinates.lng}
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 };
 
