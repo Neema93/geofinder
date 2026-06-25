c0cb120774caff0de999852ceacdbd8523aaef1a
Neema <patelneema93@gmail.com>
2026-06-25 12:18:13 -0400

    install leaflet

--- geofinder-client/src/page/Home.jsx
+++ geofinder-client/src/page/Home.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import Navbar from '../components/Navbar';
-import SearchBar from '../components/SearchBar';
-import Mapview from '../components/Mapview';
+import { useState } from "react";
+import Navbar from "../components/Navbar";
+import SearchBar from "../components/SearchBar";
+import Mapview from "../components/Mapview";
 
-   const Home = () => {
+const Home = () => {
   const [location, setLocation] = useState("");
 
   return (
@@ -15,6 +15,6 @@ import Mapview from '../components/Mapview';
 
       <Mapview location={location} />
     </>
-  )
-}
+  );
+};
 export default Home;
